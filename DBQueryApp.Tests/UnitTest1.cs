e2c71db7782a056f2c9072e3456910b40f9e3e47
Yuto Nagano <chamelion@hotmail.co.jp>
2025-04-26 19:45:19 +0900

    初回プロジェクトセットアップ

---
+++ DBQueryApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DBQueryApp.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ DBQueryApp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DBQueryApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
